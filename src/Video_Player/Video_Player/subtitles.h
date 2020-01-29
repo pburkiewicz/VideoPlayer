00c5c99e79003665d205cbdfe8ac7d693433b810
pawel <pawcio27610@wp.pl>
2020-01-29 12:34:15 +0100

    again

--- src/Video_Player/Video_Player/subtitles.h
+++ src/Video_Player/Video_Player/subtitles.h
@@ -1,4 +1,26 @@
 #ifndef SUBTITLES_H
 #define SUBTITLES_H
 
+#include <vector>
+#include <QWidget>
+
+using namespace std;
+
+struct Subtitle
+{
+    int begin;
+    vector <char> contents;
+    int end;
+};
+
+
+class Subtitles:public QWidget
+{
+    vector <Subtitle> data;
+
+    void read();
+    void search(int actual_time);
+    //void
+};
+
 #endif // SUBTITLES_H
